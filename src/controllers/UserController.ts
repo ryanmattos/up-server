9d3b000f67f64da3a4fe24cc2c32787ae6cb77b3
ryan.matos <ryan.matos@itixti.com.br>
2022-03-30 14:22:24 -0300

    feat(users): creating search route

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -3,7 +3,15 @@ import { data } from '../db/users'
 import { User } from '../models/User'
 
 export default { 
-   async getAll(req: Request, res: Response){
-      return res.status(200).json({data});
+   async getAll(req: Request, res: Response) {
+      return res.status(200).json(data);
    },
+
+   async search(req: Request, res: Response) {
+      const { name = "", email = "" } = req.body;
+      
+      const users = data.filter(user => user.name.toLowerCase().includes(name.toLowerCase()) && user.email.toLowerCase().includes(email.toLowerCase()))
+   
+      return res.status(200).json(users);
+   }
 }
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,15 @@
+import cors from 'cors';
 import express, { Router } from 'express'
 import UserController from './controllers/UserController'; './controllers/UserController'
 
 const api = express();
 const routes = Router()
 
+api.use(cors());
 api.use(express.json())
 
 routes.get('/user', UserController.getAll);
+routes.post('/user/search', UserController.search);
 
 api.use(routes)
 
